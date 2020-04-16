f0a061bd9bd9a09b818400da7fe5c4fee874f594
Taufik Kemal Thaha <taufiknackthezatzink@gmail.com>
2020-04-16 13:26:24 +0800

    tes pertama

---
+++ accounts/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+def home(request):
+    return render(request, 'accounts/dashboard.html')
+
+def product(request):
+    return HttpResponse('Product')
+
+def costumer(request):
+    return HttpResponse('Costumer')
+
